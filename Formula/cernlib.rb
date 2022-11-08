65b3c4c3f14ed211ac02770fd5543b165301fdc2
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2022-11-08 14:33:51 +0100

    Update the cernlib

--- Formula/cernlib.rb
+++ Formula/cernlib.rb
@@ -1,32 +1,32 @@
 class cernlib < Formula
   desc "Cernlib"
-  homepage "https://hepmc.web.cern.ch/"
-  url "https://hepmc.web.cern.ch/hepmc/releases/HepMC3-3.2.5.tar.gz"
-  sha256 "cd0f75c80f75549c59cc2a829ece7601c77de97cb2a5ab75790cac8e1d585032"
+  desc "CERN library"
+  homepage "https://cernlib.web.cern.ch"
+  url "https://cernlib.web.cern.ch/cernlib/download/2022_source/tar/cernlib-2022.11.08.0-free.tar.gz"
+  sha256 "733d148415ef78012ff81f21922d3bf641be7514b0242348dd0200cf1b003e46"
   license "GPL-2.0-or-later"
   revision 1
 
-  livecheck do
-    url "https://hepmc.web.cern.ch/hepmc/"
-    regex(/href=.*?cernlib[._-]v?(\d+(?:\.\d+)+)\.t/i)
-  end
-
-  depends_on "cmake" => [:build, :test]
-  depends_on "coreutils" # HepMC3-config uses greadlink
-
+  depends_on "cmake" => [:build]
+  depends_on "gcc" => [:build]
+  depends_on "coreutils" 
+  depends_on "openmotif"
+  depends_on "libXaw"
   def install
     mkdir "../build" do
       args = %W[
+        -S 
+        ../#{version}/src
+        -DCMAKE_CXX_COMPILER=g++-#{Formula["gcc"].version_suffix}
+        -DCMAKE_Fortran_COMPILER=gfortran-#{Formula["gcc"].version_suffix}
+        -DCMAKE_C_COMPILER=gcc-#{Formula["gcc"].version_suffix}
         -DCMAKE_INSTALL_PREFIX=#{prefix}
+        -DMOTIF_INCLUDE_DIR=#{Formula["openmotif"].include}
+        -DMOTIF_LIBRARIES=#{Formula["openmotif"].lib}/libXm.dylib
       ]
-
-
-      system "cmake", buildpath, *args
-      system "make"
-      system "make", "test" if build.with? "test"
-      system "make", "install"
+      system "cmake", "-B", ".", *args
+      system "cmake","--build", "."
+      system "cmake", "--install","."
     end
   end
-
-
 end
